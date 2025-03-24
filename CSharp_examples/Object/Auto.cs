d9474afcfa1eb11082d5bf272728e42a530a0b3c
borossandor27 <boros.sanyi@gmail.com>
2025-03-24 11:02:42 +0100

    1.0.10

---
+++ CSharp_examples/Object/Auto.cs
@@ -0,0 +1,28 @@
+﻿namespace Object
+{
+    class Auto
+    {
+        public string Marka { get; set; }
+        public string Rendszam { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            // Ha null vagy nem Auto típus, nem egyenlő
+            if (obj == null || !(obj is Auto))
+                return false;
+
+            // Átalakítjuk Auto típusra
+            Auto masikAuto = (Auto)obj;
+
+            // Két autó akkor egyenlő, ha a rendszámuk megegyezik
+            return this.Rendszam == masikAuto.Rendszam;
+        }
+
+        public override int GetHashCode()
+        {
+            // Fontos: ha Equals felül van írva, akkor GetHashCode is legyen!
+            return Rendszam == null ? 0 : Rendszam.GetHashCode();
+        }
+    }
+
+}
---
+++ CSharp_examples/Object/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Auto auto1 = new Auto { Marka = "Toyota", Rendszam = "ABC-123" };
+            Auto auto2 = new Auto { Marka = "BMW", Rendszam = "ABC-123" };
+            Auto auto3 = new Auto { Marka = "Toyota", Rendszam = "XYZ-999" };
+
+            Console.WriteLine(auto1.Equals(auto2)); // true – ugyanaz a rendszám
+            Console.WriteLine(auto1.Equals(auto3)); // false – más rendszám
+            Console.WriteLine(auto1.Equals(null));  // false
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadLine();
+        }
+    }
+
+}
